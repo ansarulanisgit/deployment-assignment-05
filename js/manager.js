c76a74210acd38d8c70c2d32b3c8580c63bdb27a
Ansarul Anis <ansarul.contact@gmail.comm>
2022-02-17 11:04:26 +0600

    Front-end layout is created.

--- js/manager.js
+++ js/manager.js
@@ -1,1 +1,13 @@
-console.log("Assignment Started!");
+// console.log("Assignment Started!");
+
+function getInputValue(inputId){
+    const getInput = document.getElementById(inputId);
+    const getInputText = getInput.value;
+    const inputAmount = parseFloat(getInputText);
+    return inputAmount;
+}
+
+document.getElementById('calculate').addEventListener('click', function(){
+    const income = getInputValue('income');
+    console.log(income);
+})
