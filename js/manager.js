6f8cc24b8883c782b95b82d5043bec2f1735b61e
Ansarul Anis <ansarul.contact@gmail.comm>
2022-02-17 22:53:26 +0600

    Last Deployment

--- js/manager.js
+++ js/manager.js
@@ -1,12 +1,18 @@
-
+//Function for getting input value
 function getInputValue(inputId, validationId){
     const getInput = document.getElementById(inputId);
     const getInputText = getInput.value;
     const inputAmount = parseFloat(getInputText);
-    if(isNaN(inputAmount) || inputAmount<0){
+    if(isNaN(inputAmount)){
         const errorText = document.getElementById(validationId);
         errorText.style.display = "block";
-        return false;
+        
+    }
+    else if(inputAmount<0){
+        const errorText = document.getElementById(validationId);
+        errorText.style.display = "block";
+        errorText.innerText = "Negative number is not acceptable."
+       
     }
     else{
         const errorText = document.getElementById(validationId);
@@ -16,60 +22,81 @@ function getInputValue(inputId, validationId){
     
 }
 
+//Function for making display block
 function displayBlock(id){
     const element = document.getElementById(id);
     element.style.display = 'block';
 }
 
+//Function for making display none
 function displayNone(id){
     const element = document.getElementById(id);
     element.style.display = 'none';
 }
 
+
+//function for set paragarap or span inner text
 function setInnerText(fieldId, updateValue){
     const getTextField = document.getElementById(fieldId);
     getTextField.innerText = updateValue;
 }
 
+
+//Handing calculate button event, doing calculation and update text.
 document.getElementById('calculate').addEventListener('click', function(){
     const income = getInputValue('income', 'income-error');
     const foodExpense = getInputValue('food', 'food-error');
     const rentExpense = getInputValue('rent', 'rent-error');
     const clothesExpense = getInputValue('clothes', 'clothes-error');    
     const totalExpense = foodExpense + rentExpense + clothesExpense;
+    const balanceNumber = income- totalExpense;
 
-    if(typeof(income) != "boolean" && typeof(foodExpense) != "boolean" && typeof(rentExpense) != "boolean" && typeof(clothesExpense) != "boolean"){
-    
-     if(income<totalExpense){       
-        displayBlock('income-expense-error');
+
+    if(totalExpense>income){
+        displayNone('income-expense-error');
+        displayBlock('income-low-error');
         displayNone('details-part');
     }
+    else if(isNaN(income) || isNaN(foodExpense) || isNaN(rentExpense) || isNaN(clothesExpense)){
+        displayBlock('income-expense-error');
+        displayNone('income-low-error');
+        displayNone('details-part');
+    }
+    
     else{
+        displayNone('income-low-error');
         displayNone('income-expense-error');
         displayBlock('details-part');
         setInnerText('total-expenses', totalExpense);
-        setInnerText('balance', income-totalExpense) 
-        } 
-    }
-    else{
-        displayBlock('income-expense-error');
-        displayNone('details-part');
-    }
+        setInnerText('balance', balanceNumber) 
+        }     
     })
 
+
+//Handing saving button and update the text
 document.getElementById('save-button').addEventListener('click', function(){
-    const savePercentage = getInputValue('save-percent', 'save-error');
-    if(savePercentage>100 || savePercentage<0){
+    const getSavingInput = document.getElementById('save-percent');
+    const savingInputValue = getSavingInput.value;
+    const savingPercentage = parseFloat(savingInputValue);
+
+    if(isNaN(savingPercentage)){
+        displayBlock('save-error');
+        displayNone('percentage-error');
+        displayNone('saving-part');
+    }
+    else if(savingPercentage>100 || savingPercentage<0){
+        displayNone('save-error');
         displayBlock('percentage-error');
         displayNone('saving-part');
     }
     else{
         displayNone('percentage-error');
+        displayNone('save-error');
         displayBlock('saving-part');
         const getBalanceField = document.getElementById('balance');
         const getBalanceText = getBalanceField.innerText;
         const currentBalance = parseFloat(getBalanceText);
-        const savingAmount = (currentBalance/100)*savePercentage;
+        const savingAmount = (currentBalance/100)*savingPercentage;
         const remainingBalance = currentBalance-savingAmount;
         setInnerText('saving-amount', savingAmount);
         setInnerText('remaining-balance', remainingBalance);
